044eb4db5917236d573bb668d1dd48615ba01803
fuzan <fuzan@go-goal.com>
2018-10-16 13:11:16 +0800

    build

--- src/login-main.js
+++ src/login-main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 
 import App from './pages/login/'
-
 // import crossStorage from './lib/crossStore'
 import goGoal from './goGoal/index'
+
+Vue.use(Vuex)
 
 // window.crossStorage = crossStorage
 window.goGoal = goGoal
