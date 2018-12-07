f014c10be73a457e16477e7071bda1e4cfcfd685
fuzan <fuzan@go-goal.com>
2018-12-07 10:59:27 +0800

    build

--- cdn/stocks/service-worker.js
+++ cdn/stocks/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["css/index.309d8b83.css","7ab470d3b86b209768e77280e948b9b4"],["css/stockDetail.39ae3efa.css","5b725205172a910842ab3f9972f94ae4"],["index.html","e296a196385e35cfaa81af0f91cd9cae"],["js/chunk-common.fecb1132.js","1217990231e96a0fc1d5b4dcedcd2a2d"],["js/chunk-vendors.79089207.js","543e006b3141bb779495753b7744c49f"],["js/index.d8d73923.js","d6dfd5b01061b3fa4def637bcf9d54dc"],["js/stockDetail.97df9c0c.js","523c6ee249bdf3458606b33eb678c19f"],["stockDetail.html","5c935d6e9a5515650ae55dc22d4bf361"]];
+var precacheConfig = [["css/index.309d8b83.css","7ab470d3b86b209768e77280e948b9b4"],["css/stockDetail.53cba72b.css","2b65644e6756bd3ca85348c6b8cd07e4"],["index.html","e296a196385e35cfaa81af0f91cd9cae"],["js/chunk-common.fecb1132.js","1217990231e96a0fc1d5b4dcedcd2a2d"],["js/chunk-vendors.79089207.js","543e006b3141bb779495753b7744c49f"],["js/index.d8d73923.js","d6dfd5b01061b3fa4def637bcf9d54dc"],["js/stockDetail.5885a483.js","47e7c2b1aaf2679bf9d1f7df149cc40d"],["stockDetail.html","603d69068ae86910cb66d157199479d7"]];
 var cacheName = 'sw-precache-v3-GoGoal-' + (self.registration ? self.registration.scope : '');
 
 
